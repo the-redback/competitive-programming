3faab42bad37d8488919b912dbe44aae8150b7aa
the-redback <maruf.2hin@gmail.com>
2019-02-19 01:01:44 +0600

    template updated

--- template.cpp
+++ template.cpp
@@ -1,16 +1,18 @@
-/**
- *    @author     : Maruf Tuhin
- *    @College    : CUET CSE 11
- *    @Topcoder   : the_redback
- *    @CodeForces : the_redback
- *    @UVA        : the_redback
- *    @link       : http://www.fb.com/maruf.2hin
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_  
+ *     / / / / / / /_/ / /  / /_/ / __/  
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/     
+ *                                  
+ *    @link : https://maruftuhin.com
  */
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
-typedef long long          ll;
+typedef long long ll;
 typedef unsigned long long llu;
 
 #define ft         first
@@ -19,18 +21,16 @@ typedef unsigned long long llu;
 #define pb(x)      push_back(x)
 #define all(x)     x.begin(),x.end()
 #define allr(x)    x.rbegin(),x.rend()
-#define mem(a,b)   memset(a,b,sizeof(a))
-#define repv(i,a)  for(i=0;i<(ll)a.size();i++)
-#define revv(i,a)  for(i=(ll)a.size()-1;i>=0;i--)
-#define rep(i,a,b) for(i=a;i<=b;i++)
-#define rev(i,a,b) for(i=a;i>=b;i--)
+#define mem(a, b)   memset(a,b,sizeof(a))
 #define sf(a)      scanf("%lld",&a)
-#define sf2(a,b)   scanf("%lld %lld",&a,&b)
-#define sf3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define ssf(a)     scanf("%s",&a)
+#define sf2(a, b)   scanf("%lld %lld",&a,&b)
+#define sf3(a, b, c) scanf("%lld %lld %lld",&a,&b,&c)
 #define inf        1e9
 #define eps        1e-9
 #define mod        1000000007
 #define NN         100010
+
 
 #ifdef  redback
 #define bug printf("line=%d\n",__LINE__);
@@ -42,12 +42,10 @@ struct  debugger{template<typename T>debugger& operator ,(const T& v){cerr<<v<<"
 #endif  //debugging macros
 
 
-
 int main()
 {
-    //ios_base::sync_with_stdio(0); cin.tie(0);
     #ifdef redback
-        freopen("C:\\Users\\Maruf\\Desktop\\in.txt","r",stdin);
+        freopen("input.in","r",stdin);
     #endif
 
     ll t=1,tc;
