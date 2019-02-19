c41eadaea453330560d7e64e1be5e64c3f679f4b
the-redback <maruf.2hin@gmail.com>
2019-02-20 02:43:00 +0600

    Updated workplace

--- WORKPLACE_SUBLIME/main.cpp
+++ WORKPLACE_SUBLIME/main.cpp
@@ -1,10 +1,10 @@
 /***
  *                                   ____
  *       ____ ___  ____ ________  __/ __/
- *      / __ `__ \/ __ `/ ___/ / / / /_  
- *     / / / / / / /_/ / /  / /_/ / __/  
- *    /_/ /_/ /_/\__,_/_/   \__,_/_/     
- *                                  
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
  *    @link : https://maruftuhin.com
  */
 
@@ -15,21 +15,21 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long llu;
 
-#define ft         first
-#define sd         second
-#define mp         make_pair
-#define pb(x)      push_back(x)
-#define all(x)     x.begin(),x.end()
-#define allr(x)    x.rbegin(),x.rend()
-#define mem(a, b)   memset(a,b,sizeof(a))
-#define sf(a)      scanf("%lld",&a)
-#define ssf(a)     scanf("%s",&a)
-#define sf2(a, b)   scanf("%lld %lld",&a,&b)
+#define ft           first
+#define sd           second
+#define mp           make_pair
+#define pb(x)        push_back(x)
+#define all(x)       x.begin(),x.end()
+#define allr(x)      x.rbegin(),x.rend()
+#define mem(a, b)    memset(a,b,sizeof(a))
+#define sf(a)        scanf("%lld",&a)
+#define ssf(a)       scanf("%s",&a)
+#define sf2(a, b)    scanf("%lld %lld",&a,&b)
 #define sf3(a, b, c) scanf("%lld %lld %lld",&a,&b,&c)
-#define inf        1e9
-#define eps        1e-9
-#define mod        1000000007
-#define NN         100010
+#define inf          1e9
+#define eps          1e-9
+#define mod          1000000007
+#define NN           100010
 
 
 #ifdef  redback
@@ -44,15 +44,17 @@ struct  debugger{template<typename T>debugger& operator ,(const T& v){cerr<<v<<"
 
 int main()
 {
-    #ifdef redback
-        freopen("input.in","r",stdin);
-    #endif
+#ifdef redback
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+#endif
 
-    ll t=1,tc;
+    ll t = 1, tc;
     //sf(tc);
-    ll l,m,n;
-    while(~sf(n)) {
-        ll i,j,k;
+    ll n, m;
+    while (~sf2(n,m)) {
+        ll i, j, k;
+        printf("%lld\n",n+m);
 
     }
     return 0;
--- template.cpp
+++ template.cpp
@@ -1,10 +1,10 @@
 /***
  *                                   ____
  *       ____ ___  ____ ________  __/ __/
- *      / __ `__ \/ __ `/ ___/ / / / /_  
- *     / / / / / / /_/ / /  / /_/ / __/  
- *    /_/ /_/ /_/\__,_/_/   \__,_/_/     
- *                                  
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
  *    @link : https://maruftuhin.com
  */
 
@@ -15,21 +15,21 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long llu;
 
-#define ft         first
-#define sd         second
-#define mp         make_pair
-#define pb(x)      push_back(x)
-#define all(x)     x.begin(),x.end()
-#define allr(x)    x.rbegin(),x.rend()
-#define mem(a, b)   memset(a,b,sizeof(a))
-#define sf(a)      scanf("%lld",&a)
-#define ssf(a)     scanf("%s",&a)
-#define sf2(a, b)   scanf("%lld %lld",&a,&b)
+#define ft           first
+#define sd           second
+#define mp           make_pair
+#define pb(x)        push_back(x)
+#define all(x)       x.begin(),x.end()
+#define allr(x)      x.rbegin(),x.rend()
+#define mem(a, b)    memset(a,b,sizeof(a))
+#define sf(a)        scanf("%lld",&a)
+#define ssf(a)       scanf("%s",&a)
+#define sf2(a, b)    scanf("%lld %lld",&a,&b)
 #define sf3(a, b, c) scanf("%lld %lld %lld",&a,&b,&c)
-#define inf        1e9
-#define eps        1e-9
-#define mod        1000000007
-#define NN         100010
+#define inf          1e9
+#define eps          1e-9
+#define mod          1000000007
+#define NN           100010
 
 
 #ifdef  redback
