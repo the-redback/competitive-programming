691413966eab36933b18a71f213224d51790716c
the-redback <maruf.2hin@gmail.com>
2020-04-26 00:50:18 +0600

    Codeforces A and B

---
+++ workspace-codeforces/1341-Codeforces_Round_#637_(Div._2)_-_Thanks,_Ivan_Belonogov!/A/A.cpp
@@ -0,0 +1,51 @@
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
+ *    @link : https://maruftuhin.com
+ */
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+#define mem(a, b) memset(a, b, sizeof(a))
+#define inf       1e9
+#define eps       1e-9
+#define mod       1000000007
+#define NN        100010
+
+// clang-format off
+#ifdef  redback
+#define bug printf("line=%d\n",__LINE__);
+#define debug(args...) {cout<<":: "; dbg,args; cout<<endl;}
+struct  debugger {template<typename T>debugger& operator , (const T& v) {cout << v << " "; return *this;}} dbg;
+#else
+#define bug
+#define debug(args...)
+#endif  //debugging macros
+// clang-format on
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1, tc;
+    cin >> tc;
+    ll n, m, c, d, a, b;
+    while (tc--) {
+        cin >> n >> a >> b >> c >> d;
+        bool flag = 0;
+
+        if (n * (a - b) <= c + d && n * (a + b) >= c - d) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
