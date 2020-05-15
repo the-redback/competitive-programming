833eca15ba9fc62c6715125b8104cc4e538ebb44
the-redback <maruf.2hin@gmail.com>
2020-05-15 17:00:09 +0600

    Solved B

---
+++ workspace-codeforces/1353-Codeforces_Round_#642_(Div._3)/B/B.cpp
@@ -0,0 +1,82 @@
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
+ *    @link : https://maruftuhin.com
+ */
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+#define mem(a, b) memset(a, b, sizeof(a))
+#define inf       1e9
+#define eps       1e-9
+#define mod       1000000007
+#define NN        200010
+
+// clang-format off
+#ifdef  redback
+#define bug printf("line=%d\n",__LINE__);
+#define debug(args...) {cout<<":: "; dbg,args; cout<<endl;}
+struct  debugger {template<typename T>debugger& operator , (const T& v) {cout << v << " "; return *this;}} dbg;
+#else
+#define bug
+#define debug(args...)
+#endif  //debugging macros
+// clang-format on
+
+ll a[10000], b[1000];
+
+void solve(ll tc) {
+    ll i, j, k, n;
+    cin >> n >> k;
+
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    sort(a, a + n);
+    sort(b, b + n);
+
+    ll sum = 0;
+
+    i = 0;
+    j = n - 1;
+    for (ll l = 0; l < k; l++) {
+        if (a[i] >= b[j]) {
+            break;
+        }
+        swap(a[i], b[j]);
+        i++;
+        j--;
+    }
+
+    for (i = 0; i < n; i++) {
+        sum += a[i];
+    }
+
+    cout << sum << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1, tc;
+    cin >> tc;
+    ll n, m;
+    while (tc--) {
+        solve(t++);
+    }
+    return 0;
+}
