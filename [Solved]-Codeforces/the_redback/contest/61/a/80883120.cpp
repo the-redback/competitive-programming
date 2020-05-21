6412f1da1a818402df1b96cdd2c2078e1e6467c1
the-redback <maruf.2hin@gmail.com>
2020-05-22 01:17:36 +0600

    Pulled more solved problem of codeforces

---
+++ [Solved]-Codeforces/the_redback/contest/61/a/80883120.cpp
@@ -0,0 +1,61 @@
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
+ *    @link : https://maruftuhin.com
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define mem(a, b) memset(a, b, sizeof(a))
+#define inf       1e9
+#define eps       1e-9
+#define mod       1000000007
+#define NN        200010
+
+// clang-format off
+#ifdef  redback
+#define bug printf("line=%d\n",__LINE__);
+#define debug(args...) {cout<<":: "; dbg,args; cout<<endl;}
+struct  debugger {template<typename T>debugger& operator , (const T& v) {cout << v << " "; return *this;}} dbg;
+#else
+#define bug
+#define debug(args...)
+#endif  //debugging macros
+// clang-format on
+
+void solve(ll t) {
+    ll i, j, k, n;
+    cin >> n;
+
+    cout << n << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1, tc;
+    // cin >> tc;
+    ll     n, m;
+    string str1, str2;
+    while (cin >> str1 >> str2) {
+        string str;
+        for (ll i = 0; i < str1.size(); i++) {
+            if (str1[i] != str2[i]) {
+                str += "1";
+            } else {
+                str += "0";
+            }
+        }
+        cout << str;
+    }
+    return 0;
+}
