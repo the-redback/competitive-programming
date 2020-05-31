ea072e1afc20e97e07137ca1b5a5c02abeb54fc6
the-redback <maruf.2hin@gmail.com>
2020-05-31 20:04:09 +0600

    Solved 319/b

---
+++ workspace-codeforces/contests/319/b/b.cpp
@@ -0,0 +1,92 @@
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
+ *    @link : https://maruftuhin.com
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define xx        first
+#define yy        second
+#define pb(x)     push_back(x)
+#define all(x)    x.begin(), x.end()
+#define allr(x)   x.rbegin(), x.rend()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define inf       1e9
+#define eps       1e-9
+#define mod       1000000007
+#define NN        100010
+
+/* -------------------------------------------------------------------------- */
+/*                              Debugging Macros                              */
+/* -------------------------------------------------------------------------- */
+#ifdef redback
+#include "prettyprint.hpp"
+#define dbg(...)                        \
+    cout << "L-" << __LINE__ << " :: "; \
+    _print_out(#__VA_ARGS__, __VA_ARGS__);
+template <typename T>
+void _print_out(const char* name, T&& arg1) {
+    cout << name << " = " << arg1 << endl;
+}
+template <typename T1, typename... Args>
+void _print_out(const char* names, T1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " = " << arg1 << " | ";
+    _print_out(comma + 1, args...);
+}
+template <typename T>
+void _print_out(const char* name, T a[], int n) {
+    cout << name << " = " << pretty_print_array(a, n) << endl;
+}
+#else
+#define dbg(args...)
+#endif
+/* -------------------------------------------------------------------------- */
+
+void solve(ll n) {
+    ll i, j, k;
+    ll m;
+
+    stack<ll> st;
+
+    ll ans = 0;
+    vector<ll> v(100010);
+    for (i = 0; i < n; i++) {
+        cin >> k;
+        while (!st.empty() && st.top() < k) {
+            v[k] = max(v[k], v[st.top()] + 1);
+            st.pop();
+        }
+
+        if (st.empty()) {
+            v[k] = -1;
+        }
+
+        st.push(k);
+
+        ans = max(ans, v[k] + 1);
+    }
+
+    cout << ans << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1, tc;
+    // cin >> tc;
+    ll n, m;
+    while (cin >> n) {
+        solve(n);
+    }
+    return 0;
+}
