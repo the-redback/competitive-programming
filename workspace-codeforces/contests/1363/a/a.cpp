471a5dacce871657314b05ef403912e23481f494
the-redback <maruf.2hin@gmail.com>
2020-06-01 17:39:20 +0600

    codeforcces 1363 contest done

---
+++ workspace-codeforces/contests/1363/a/a.cpp
@@ -0,0 +1,97 @@
+/***
+ *                                   ____
+ *       ____ ___  ____ ________  __/ __/
+ *      / __ `__ \/ __ `/ ___/ / / / /_
+ *     / / / / / / /_/ / /  / /_/ / __/
+ *    /_/ /_/ /_/\__,_/_/   \__,_/_/
+ *
+ *    @link : https://maruftuhin.com
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define xx        first
+#define yy        second
+#define pb(x)     push_back(x)
+#define all(x)    x.begin(), x.end()
+#define allr(x)   x.rbegin(), x.rend()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define inf       1e9
+#define eps       1e-9
+#define mod       1000000007
+#define NN        100010
+
+/* -------------------------------------------------------------------------- */
+/*                              Debugging Macros                              */
+/* -------------------------------------------------------------------------- */
+#ifdef redback
+#include "prettyprint.hpp"
+#define dbg(...)                        \
+    cout << "L-" << __LINE__ << " :: "; \
+    _print_out(#__VA_ARGS__, __VA_ARGS__);
+template <typename T>
+void _print_out(const char* name, T&& arg1) {
+    cout << name << " = " << arg1 << endl;
+}
+template <typename T1, typename... Args>
+void _print_out(const char* names, T1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " = " << arg1 << " | ";
+    _print_out(comma + 1, args...);
+}
+template <typename T>
+void _print_out(const char* name, T a[], int n) {
+    cout << name << " = " << pretty_print_array(a, n) << endl;
+}
+#else
+#define dbg(args...)
+#endif
+/* -------------------------------------------------------------------------- */
+
+void solve() {
+    ll i, j, k;
+    ll n, m;
+    cin >> n >> m;
+
+    ll even = 0, odd = 0;
+
+    for (i = 0; i < n; i++) {
+        cin >> k;
+        if (k % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    if (odd % 2 == 0) {
+        odd--;
+    }
+
+    if(even == 0 && m%2 == 0){
+        cout << "No\n";
+        return;
+    }
+
+    if (odd >= 1 && m - odd <= even) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1, tc;
+    cin >> tc;
+    ll n, m;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
