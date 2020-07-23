550a20bb7f5cb8e89c171b37a656c97b3de97b98
the-redback <maruf.2hin@gmail.com>
2020-07-24 05:32:48 +0600

    Yay!! Solved 2500-level problem!

--- workspace-codeforces/contests/1382/e/e.cpp
+++ workspace-codeforces/contests/1382/e/e.cpp
@@ -33,10 +33,105 @@ const ll NN = 1e5 + 10;
 
 void solve() {
     ll i, j, k;
-    ll n, m, ans = 0;
-    cin >> n;
+    ll n, m;
+    ll x, y;
+    cin >> n >> x >> y;
 
-    cout << ans << "\n";
+    vector<ll> v[n + 2], ans(n), vis(n + 2), a(n);
+    priority_queue<pair<int, int>> pq;
+
+    for (i = 0; i < n; i++) {
+        cin >> k;
+        a[i] = k;
+        v[k].pb(i);
+        vis[k]++;
+    }
+    ll in = 0, sum = n;
+    for (i = 1; i <= n + 1; i++) {
+        if (vis[i] > 0) {
+            pq.push({vis[i], i});
+        }
+        if (vis[i] == 0) {
+            in = i;
+        }
+    }
+
+    k = y - x;
+
+    while (x > 0 && pq.size()) {
+        pair<ll, ll> pi;
+        pi = pq.top();
+        pq.pop();
+
+        ll ii = v[pi.second].back();
+        v[pi.second].pop_back();
+        sum--;
+
+        ans[ii] = pi.second;
+        pi.first--;
+        if (pi.first > 0) pq.push(pi);
+        x--;
+    }
+
+    while (pq.size() >= 2 && k > 0) {
+        if (k == 3 && pq.size() == 3 && sum == 3) break;
+        pair<ll, ll> pi, pj;
+        pi = pq.top();
+        pq.pop();
+        pj = pq.top();
+        pq.pop();
+
+        ll ii = v[pi.second].back();
+        ll jj = v[pj.second].back();
+        v[pi.second].pop_back();
+        v[pj.second].pop_back();
+        sum -= 2;
+
+        ans[ii] = a[jj];
+        k--;
+        if (k > 0) {
+            ans[jj] = a[ii];
+            k--;
+        }
+        pi.first--;
+        pj.first--;
+        if (pi.first > 0) pq.push(pi);
+        if (pj.first > 0) pq.push(pj);
+    }
+
+    if (k == 3) {
+        vector<pair<ll, ll>> s;
+        for (i = 0; i <= n + 1 && s.size() < 3; i++) {
+            if (v[i].size()) {
+                s.emplace_back(i, v[i].back());
+                v[i].pop_back();
+            }
+        }
+        if (s.size() < 3) {
+            cout << "NO\n";
+            return;
+        }
+        ans[s[2].second] = s[0].first;
+        ans[s[1].second] = s[2].first;
+        ans[s[0].second] = s[1].first;
+        k -= 3;
+    }
+
+    if (k > 0 || x > 0) {
+        cout << "NO\n";
+        return;
+    }
+
+    for (i = 0; i < n; i++) {
+        if (ans[i] == 0) ans[i] = in;
+    }
+
+    cout << "YES\n";
+    for (i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+
     return;
 }
 
